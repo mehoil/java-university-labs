60de04154daaa636a96a78066780320ed68e565e
Dorokhov Mikhail <129596462+mehoil@users.noreply.github.com>
2025-03-22 19:06:05 +0300

    Initial commit

---
+++ lab1-container-class/src/main/java/ru/mehoil/Main.java
@@ -0,0 +1,13 @@
+package ru.mehoil;
+
+/**
+ * Application entry point.
+ *
+ * @author Mikhail Dorokhov
+ * @version 1.0
+ */
+public class Main {
+    public static void main(final String[] args) {
+        System.out.println("Hello world!");
+    }
+}
