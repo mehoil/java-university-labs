2469af964ee943ee2280549fda18ab26e859fafe
mehoil <129596462+mehoil@users.noreply.github.com>
2025-05-13 21:30:28 +0300

    lab5 initial

---
+++ lab5-reflection-api/src/main/java/ru/mehoil/ReflectionApiLab.java
@@ -0,0 +1,21 @@
+package ru.mehoil;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.IntConsumer;
+import java.util.stream.IntStream;
+
+/**
+ * Application entry point.
+ *
+ * @author Mikhail Dorokhov
+ * @version 1.0
+ */
+public class ReflectionApiLab {
+
+    public static void main(final String[] args) {
+
+    }
+
+}
